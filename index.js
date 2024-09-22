1dc88446b5191452ec52aacdfd8e6ac0c0bc1ad5
Chamodpw2000 <143950123+Chamodpw2000@users.noreply.github.com>
2024-09-22 13:53:04 +0530

    bugs fixed

--- index.js
+++ index.js
@@ -10,20 +10,13 @@ import {Router} from './routes/routes.js'
 
 const app = express()
 app.use(express.json())
-app.use(cors({
-  origin: 'https://contactms-client-seven.vercel.app',
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true,
-}))
+app.use(cors())
 dotenv.config({path: "./config/.env"})
 
-app.use(cors(corsOptions));
-
 // Handle preflight requests
-app.options('*', cors(corsOptions));
 
-app.use("/contactms",Router)
 
+app.use("/contactmsyt",Router)
 app.listen(process.env.PORT, () => {
   console.log('Server is listening on port 3000')
 })
